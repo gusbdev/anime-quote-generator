bb3cfad33ecc0ffcafd8b37ff604ea8a1144470a
Gus <gusbdev@gmail.com>
2021-09-16 23:30:42 -0300

    Config routes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,39 @@
-function App() {
-  return <div className="App"></div>;
-}
+
+import { useCallback, useEffect } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { useSetRecoilState } from "recoil";
+import axios from "axios";
+import { animeTitles } from "./store";
+import Homepage from "./pages/home";
+import Animepage from "./pages/anime";
+
+const App = () => {
+	const setTitles = useSetRecoilState(animeTitles);
+
+	const fetchAnimes = useCallback(async () => {
+		try {
+			const res = await axios.get(
+				"https://animechan.vercel.app/api/available/anime"
+			);
+
+			setTitles(res?.data);
+		} catch (error) {
+			console.log(error?.response?.data?.error);
+		}
+	}, [setTitles]);
+
+	useEffect(() => {
+		fetchAnimes();
+	}, [fetchAnimes]);
+
+	return (
+		<BrowserRouter>
+			<Switch>
+				<Route exact path="/" component={Homepage} />
+				<Route exact path="/anime/:name" component={Animepage} />
+			</Switch>
+		</BrowserRouter>
+	);
+};
 
 export default App;
